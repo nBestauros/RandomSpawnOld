58195efb7ec461403bcb0c04b02d60b93c2d2cef
Nic <nbestauros@gmail.com>
2019-09-28 23:13:19 -0700

    Fixed issue when bed was obstructed

--- src/com/Bestauros/RandomSpawn/NewSpawnListener.java
+++ src/com/Bestauros/RandomSpawn/NewSpawnListener.java
@@ -18,50 +18,54 @@ public class NewSpawnListener implements Listener {
     @EventHandler (priority = EventPriority.HIGHEST)
     public void onPlayerSpawn(PlayerSpawnLocationEvent sle){
         //checks to see if player has played before
-        if(!sle.getPlayer().hasPlayedBefore()){
-            ArrayList<Biome> forbiddenBiomeList = new ArrayList<Biome>();
-            //TODO: Dont hardcode these, add them to the config file
-            forbiddenBiomeList.add(Biome.OCEAN);
-            forbiddenBiomeList.add(Biome.COLD_OCEAN);
-            forbiddenBiomeList.add(Biome.DEEP_COLD_OCEAN);
-            forbiddenBiomeList.add(Biome.DEEP_FROZEN_OCEAN);
-            forbiddenBiomeList.add(Biome.DEEP_LUKEWARM_OCEAN);
-            forbiddenBiomeList.add(Biome.DEEP_OCEAN);
-            forbiddenBiomeList.add(Biome.DEEP_WARM_OCEAN);
-            forbiddenBiomeList.add(Biome.FROZEN_OCEAN);
-            forbiddenBiomeList.add(Biome.LUKEWARM_OCEAN);
-            forbiddenBiomeList.add(Biome.WARM_OCEAN);
-
-            List<World> worldList = Bukkit.getWorlds();
-            //TODO: take user input for world name
-            for(World w : worldList){
-                if(w.getName().equals("world")){
-                    world = w;
-                }
-            }
-
-            double x;
-            double y;
-            double z;
-
-            Random random = new Random();
-
-            boolean isForbidden = false;
-            do {
-                //random num up to 1500 times random either -1 or 1
-                //TODO: take a range from config file
-                x = random.nextInt(300) * (random .nextBoolean() ? -1 : 1);
-                z = random.nextInt(300) * (random .nextBoolean() ? -1 : 1);
-                Biome temp = world.getBiome((int) x, (int) z);
-                for(int i = 0; i <forbiddenBiomeList.size(); i++){
-                    if(temp.equals(forbiddenBiomeList.get(i))){
-                        isForbidden=true;
-                    }
-                }
-            }while(isForbidden);
-            y = world.getHighestBlockYAt((int) x, (int) z);
-            sle.setSpawnLocation(new Location(world,x,y,z));
+        if(!sle.getPlayer().hasPlayedBefore() && sle.getPlayer().getBedSpawnLocation() == null){
+            GetResLoc(sle);
             //Bukkit.broadcastMessage(x+" "+ y+" "+ " "+z);
         }
+    }
+
+    private void GetResLoc(PlayerSpawnLocationEvent sle) {
+        ArrayList<Biome> forbiddenBiomeList = new ArrayList<Biome>();
+        //TODO: Dont hardcode these, add them to the config file
+        forbiddenBiomeList.add(Biome.OCEAN);
+        forbiddenBiomeList.add(Biome.COLD_OCEAN);
+        forbiddenBiomeList.add(Biome.DEEP_COLD_OCEAN);
+        forbiddenBiomeList.add(Biome.DEEP_FROZEN_OCEAN);
+        forbiddenBiomeList.add(Biome.DEEP_LUKEWARM_OCEAN);
+        forbiddenBiomeList.add(Biome.DEEP_OCEAN);
+        forbiddenBiomeList.add(Biome.DEEP_WARM_OCEAN);
+        forbiddenBiomeList.add(Biome.FROZEN_OCEAN);
+        forbiddenBiomeList.add(Biome.LUKEWARM_OCEAN);
+        forbiddenBiomeList.add(Biome.WARM_OCEAN);
+
+        List<World> worldList = Bukkit.getWorlds();
+        //TODO: take user input for world name
+        for(World w : worldList){
+            if(w.getName().equals("world")){
+                world = w;
+            }
+        }
+
+        double x;
+        double y;
+        double z;
+
+        Random random = new Random();
+
+        boolean isForbidden = false;
+        do {
+            //random num up to 1500 times random either -1 or 1
+            //TODO: take a range from config file
+            x = random.nextInt(300) * (random .nextBoolean() ? -1 : 1);
+            z = random.nextInt(300) * (random .nextBoolean() ? -1 : 1);
+            Biome temp = world.getBiome((int) x, (int) z);
+            for(int i = 0; i <forbiddenBiomeList.size(); i++){
+                if(temp.equals(forbiddenBiomeList.get(i))){
+                    isForbidden=true;
+                }
+            }
+        }while(isForbidden);
+        y = world.getHighestBlockYAt((int) x, (int) z);
+        sle.setSpawnLocation(new Location(world,x,y,z));
     }
 }
